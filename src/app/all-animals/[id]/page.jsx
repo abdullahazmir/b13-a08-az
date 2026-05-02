7a35db28c95e318c53a378d26b603a1409c51530
abdullahazmir <ab.azmir@gmail.com>
2026-05-02 20:03:15 +0600

    authentication starts

--- src/app/all-animals/[id]/page.jsx
+++ src/app/all-animals/[id]/page.jsx
@@ -1,27 +1,40 @@
 import { Button, Card, Chip, Separator } from "@heroui/react";
 import Image from "next/image";
 import Link from "next/link";
-import { BiDownload, BiLocationPlus } from "react-icons/bi";
-import { FaHeart } from "react-icons/fa6";
+import { BiLocationPlus } from "react-icons/bi";
 import { IoPricetag } from "react-icons/io5";
-const AnimalCard = ({animal}) => {
-    return (
-        <Card className="border rounded-xl">
-            <div className="relative w-full aspect-square">
-            <Image
-                src={animal.image}
-                fill
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                alt={animal.name}
-                className="object-cover rounded-xl"
-                />
 
+const AnimalDetailsPage = async ({params}) => {
+    const {id} = await params;
+    const res = await fetch('https://b13-a08-az2.vercel.app/data.json')
+    const animals = await res.json()
+
+    const animal = animals.find(a => a.id == id)
+
+   
+
+    return (
+        <div className="flex justify-between max-w-106 gap-20 mx-auto">
+            <Image src={animal.image}
+            alt="animal.image"
+            width={700}
+            height={700} 
+             className="rounded-3xl"
+             >
+               
+
+            </Image>
+        <div>
+            
+            <div className="relative w-full aspect-square">
+           
                 <Chip size="sm" className="absolute right-2 top-2">{animal.breed}</Chip>
             </div>
 
             <div>
                 <h2 className="font-medium">{animal.name}</h2>
             </div>
+  <p>{animal.description}</p>
 
            <div className="flex gap-5">
              <div className="flex items-center gap-2">
@@ -38,10 +51,12 @@ const AnimalCard = ({animal}) => {
             </div>
            </div>
 
-     <Link href={`/all-animals/${animal.id}`}>      <Button variant="outline" className={'w-full'}>View</Button></Link>
+    
             
-        </Card>
+        
+        </div>
+        </div>
     );
 };
 
-export default AnimalCard;
+export default AnimalDetailsPage;

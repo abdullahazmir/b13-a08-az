c05dfd6498cca87c1011091c6892d50ac81a73f7
abdullahazmir <ab.azmir@gmail.com>
2026-05-02 12:19:53 +0600

    navbar design done

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+   images: {
+    remotePatterns: [
+      {
+        hostname: 'bdbusinessfinder.com'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
+
+// https://bdbusinessfinder.com/wp-content/uploads/2023/06/11-32.jpg
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,14 @@ const Navbar = () => {
       <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
         <div className="flex gap-2 items-center">
           <Image
-            src={"/logo.png"}
+            src={"/qurbani2.jpg"}
             alt="logo"
             loading="eager"
             width={30}
             height={30}
             className="object-cover h-auto w-auto"
           />
-          <h3 className="font-black text-lg">pixgen.</h3>
+          <h3 className="font-black text-lg">QURBANI HAT</h3>
         </div>
 
         <ul className="flex items-center gap-5 text-sm">
@@ -23,18 +23,16 @@ const Navbar = () => {
             <Link href={"/"}>Home</Link>
           </li>
           <li>
-            <Link href={"/all-photos"}>All Photos</Link>
+            <Link href={"/all-photos"}>All Animals</Link>
           </li>
-          <li>
-            <Link href={"/pricing"}>Pricing</Link>
-          </li>
+        
           <li>
             <Link href={"/profile"}>Profile</Link>
           </li>
         </ul>
 
-        <div className="flex gap-4">
-          <ul className="flex items-center  text-sm">
+        <div >
+          <ul className="flex items-center  gap-4 text-sm">
             <li>
               <Link href={"/signup"}>SignUp</Link>
             </li>
